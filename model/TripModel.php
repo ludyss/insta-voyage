fb7c477221a0e3d3b9a9545bee0d078d77821747
CaptainFalcon92 <captainfalcon92@live.fr>
2014-03-27 09:47:48 +0100

    temp

--- model/TripModel.php
+++ model/TripModel.php
@@ -1,6 +1,6 @@
 <?php
 
-require './model/BaseModel.php';
+require_once './model/BaseModel.php';
 
 /**
  * Description of TripModel
@@ -8,36 +8,5 @@ require './model/BaseModel.php';
  */
 class TripModel extends BaseModel
 {
-
-    public function findAll(array $columns = array(), $offset = null, $limit = null)
-    {
-        $sql = 'SELECT * FROM `Trip`';
-        
-        
-        
-        $query = $this->db->prepare($sql);
-
-        $query->execute(array(':id' => $id));
-        return $query->fetch();
-    }
-
-    public function findById($id, array $columns = array())
-    {
-        $sql = 'SELECT * FROM `Trip` WHERE `id_trip` = :id';
-        $query = $this->db->prepare($sql);
-
-        $query->execute(array(':id' => $id));
-        return $query->fetch();
-    }
-
-    public function save(array $trip)
-    {
-        
-    }
-
-    public function deleteById($id, $hard = false)
-    {
-        
-    }
 
 }
